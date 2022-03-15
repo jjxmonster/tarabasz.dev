9514f086e453330953bdead044677be7d4d3a1b9
jjxmonster <kuba.tarabasz@wp.pl>
2022-03-15 13:59:21 +0100

    improve theme toggle button style

--- components/ThemeToggleButton/index.js
+++ components/ThemeToggleButton/index.js
@@ -5,14 +5,14 @@ import { ThemeContext } from '../../services/theme/ThemeContext.js';
 const ThemeToggleButton = () => {
    const { theme, setTheme } = useContext(ThemeContext);
    return (
-      <div className='transition duration-500 ease-in-out rounded-full p-2'>
+      <div className='transition duration-500 ease-in-out rounded-full p-2 overflow-hidden'>
          <div
             onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
             className={`w-8 h-8 rounded-full relative pointer ${
                theme === 'dark' ? 'darkmode' : 'lightmode'
-            } before:absolute before:w-6 before:h-6 overflow-hidden cursor-pointer before:-top-px before:-left-px	 before:bg-gray before:duration-500 before:rounded-full before:duration:500 ${
+            } before:absolute before:w-7 before:h-7  cursor-pointer before:-top-1 before:-left-1 before:bg-gray before:duration-500 before:rounded-full before:duration:500 ${
                theme === 'light' &&
-               'before:-translate-x-8 before:-translate-y-8'
+               'before:-translate-x-8 before:-translate-y-8 '
             } `}
          ></div>
       </div>
